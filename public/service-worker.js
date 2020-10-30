f6dbf0d300e5a5540e5f4439e0d1e2f43e730c8d
TJ <tahmeenaowais@yahoo.com>
2020-10-30 18:54:05 -0400

    fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,7 +47,7 @@ self.addEventListener("activate", function(evt) {
 // fetch
 self.addEventListener("fetch", function(evt) {
   const {url} = evt.request;
-  if (url.includes("/api")) {
+  if (url.includes("/api/transaction")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+//const compression = require("compression");
 
-const PORT = process.env.PORT ||8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
+//app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
