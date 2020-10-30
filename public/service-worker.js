52544ea120ce8e2d1b65968db9d5f94cb5aaf073
TJ <tahmeenaowais@yahoo.com>
2020-10-30 18:55:56 -0400

    push to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,7 +47,7 @@ self.addEventListener("activate", function(evt) {
 // fetch
 self.addEventListener("fetch", function(evt) {
   const {url} = evt.request;
-  if (url.includes("/api/transaction")) {
+  if (url.includes("/api")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
