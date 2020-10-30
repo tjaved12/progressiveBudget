c800dd7d1bc8ae267f4f224b3d37c9a8f8ee89b9
TJ <tahmeenaowais@yahoo.com>
2020-10-30 18:06:41 -0400

    fixed the service file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,6 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-
-
 const staticFilesToPreCache = [
   "/",
   "/index.js",
@@ -11,7 +9,7 @@ const staticFilesToPreCache = [
   "/styles.css",
   "icons/icon-192x192.png",
   "icons/icon-512x512.png",
-  "/api/transaction"
+  // "/api/transaction"
 
   ]
 
